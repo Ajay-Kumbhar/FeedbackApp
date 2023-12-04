dd5063d1bc70d9ef28dcb6b025527f60a4819d6b
Ajay-Kumbhar <93241196+Ajay-Kumbhar@users.noreply.github.com>
2023-12-04 12:07:19 +0530

    Create header.php

---
+++ inc/header.php
@@ -0,0 +1,22 @@
+<?php include 'config/database.php'?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-12 col-md-10 col-lg-8 d-flex py-3">
+                <a class="navbar-brand fs-4 fw-normal" href="#">Traversy Media</a>
+                <nav class="nav ms-auto">
+                    <a class="nav-link text-secondary" href="index.php">Home</a>
+                    <a class="nav-link text-secondary" href="feedback.php">Feedback</a>
+                    <a class="nav-link text-secondary" href="about.php">About</a>
+                </nav>
+            </div>
+        </div>
