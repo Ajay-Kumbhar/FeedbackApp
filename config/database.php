50cbc2f883a288b3370d87cc052c8cd95e9ce006
Ajay-Kumbhar <93241196+Ajay-Kumbhar@users.noreply.github.com>
2023-12-04 12:06:33 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,1 +1,13 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'ajay');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'php_dev');
 
+    $conn = new \mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if($conn->connect_error){
+        die('Connection failed').$conn->connect_error;
+    }
+
+?>
